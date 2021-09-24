b083ad147e2e65d5b70a1986d1bd30be4cb9c8c0
Emil Örjes <emil.orjes@hotmail.com>
2021-09-24 22:17:02 +0200

    Addad Global.css and imported google font in global folder. Imported Global.css in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes } from "./routes/Routes";
 import { Navigations } from "./components/navigations/Navigations";
 import { PokemonProvider } from "./shared/provider/PokemonProvider";
+import "./shared/global/Global.css"
 
 function App() {
   return (

f994b454722ad621111fdad196e98bc74a9a7d7a
Emil Örjes <emil.orjes@hotmail.com>
2021-09-24 15:46:03 +0200

    Added a fetchData function that is async and implemented a try catch

--- src/shared/provider/PokemonProvider.js
+++ src/shared/provider/PokemonProvider.js
@@ -7,6 +7,15 @@ export const PokemonProvider = () => {
 
     const[serverData, setServerData] = useState()
     const[loading, setLoading] = useState(true)
+
+    const fetchDAta = async () =>{
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
     return (
         <div>
             

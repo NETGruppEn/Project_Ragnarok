3968bde3318ccf3fb50ef77ab92665a457f10084
Emil Örjes <emil.orjes@hotmail.com>
2021-09-24 14:55:41 +0200

    Imported useLocation in TodaysDateView to reach the states that where pushed from the button in DesktopNavigation

--- src/views/todaysdateview/TodaysDateView.js
+++ src/views/todaysdateview/TodaysDateView.js
@@ -1,7 +1,12 @@
+import { useLocation } from "react-router"
+
 export const TodaysDateView = () =>{
+
+    const location = useLocation()
     return (
         <div>
             <h1>Todays date</h1>
+            <h2>{location.state.welcomeMessage} {location.state.date}</h2>
         </div>
     )
 }

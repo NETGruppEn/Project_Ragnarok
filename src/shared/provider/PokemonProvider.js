0f06f8b38e7e725a02b443ad2895097588d2d712
Emil Örjes <emil.orjes@hotmail.com>
2021-09-24 16:11:54 +0200

    Return PokemonContext.Provider and put serverData and loading as global values

--- src/shared/provider/PokemonProvider.js
+++ src/shared/provider/PokemonProvider.js
@@ -1,31 +1,30 @@
 import { useState, createContext, useEffect } from "react";
 import PokemonAPIService from "../api/service/PokemonAPIService";
 
-export const PokemonContext = createContext()
+export const PokemonContext = createContext();
 
-export const PokemonProvider = () => {
+export const PokemonProvider = ({ children }) => {
+  const [serverData, setServerData] = useState();
+  const [loading, setLoading] = useState(true);
 
-    const[serverData, setServerData] = useState()
-    const[loading, setLoading] = useState(true)
-
-    const fetchData = async () =>{
-        setLoading(true)
-        try {
-            const {data} = await PokemonAPIService.getHundredPokemon()
-            setServerData(data)
-            setLoading(false)
-        } catch (error) {
-            console.log("Error with API: " + error)
-        }
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const { data } = await PokemonAPIService.getHundredPokemon();
+      setServerData(data);
+      setLoading(false);
+    } catch (error) {
+      console.log("Error with API: " + error);
     }
+  };
 
-    useEffect(() => {
-        fetchData();
-      }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <PokemonContext.Provider value={{ serverData, loading }}>
+      {children}
+    </PokemonContext.Provider>
+  );
+};

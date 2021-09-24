48f5946bd153d5d5303d85ba722bb9df3f413d83
Emil Örjes <emil.orjes@hotmail.com>
2021-09-24 14:29:30 +0200

    Added two buttons in DesktopNavigation. When a button is  pressed user i sent to a specific view with use of useHistory (history.push())

---
+++ src/components/navigations/desktopnavigation/DesktopNavigation.js
@@ -0,0 +1,14 @@
+import { useHistory } from "react-router";
+import RoutingPath from "../../routes/RoutingPath";
+
+export const DesktopNavigation = () => {
+
+const history = useHistory()
+
+    return (
+        <div>
+            <button onClick={ () => history.push(RoutingPath.homeView)}>Home</button>
+            <button onClick={ () => history.push(RoutingPath.todaysDateView)}>Todays Date</button>
+        </div>
+    )
+}

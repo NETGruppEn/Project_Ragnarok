e7936c74424cbf57cb993821fbee5472c29e7a2b
Emil Örjes <emil.orjes@hotmail.com>
2021-09-24 15:33:44 +0200

    Created a PokemonContext and a RAFC. ALso created two useState()/ serverData and loading

--- src/shared/provider/PokemonProvider.js
+++ src/shared/provider/PokemonProvider.js
@@ -1,2 +1,15 @@
-import { useState,createContext, useEffect } from "react";
+import { useState, createContext, useEffect } from "react";
 import PokemonAPIService from "../api/service/PokemonAPIService";
+
+export const PokemonContext = createContext()
+
+export const PokemonProvider = () => {
+
+    const[serverData, setServerData] = useState()
+    const[loading, setLoading] = useState(true)
+    return (
+        <div>
+            
+        </div>
+    )
+}

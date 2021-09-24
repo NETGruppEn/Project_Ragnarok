668d467a3fefdc7fdd6aac8e78265437eca1c6b6
Emil Örjes <emil.orjes@hotmail.com>
2021-09-24 13:42:12 +0200

    Added RAFC and a <h1> to HomeView.js and TodaysDate.js

---
+++ src/views/homeview/HomeView.js
@@ -0,0 +1,7 @@
+export const HomeView = () => {
+    return (
+        <div>
+            <h1>List of Pokemon</h1>
+        </div>
+    )
+}
---
+++ src/views/todaysdate/TodaysDate.js
@@ -0,0 +1,7 @@
+export const TodaysDate = () =>{
+    return (
+        <div>
+            <h1>Todays date</h1>
+        </div>
+    )
+}

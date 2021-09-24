3c30ef4fd4ecff084a073bb95d6d19f202d86b49
Emil Örjes <emil.orjes@hotmail.com>
2021-09-24 14:44:19 +0200

    Created a object with a welcomeMessage and a date that is pushed when Todays Date button is pressed

--- src/components/navigations/desktopnavigation/DesktopNavigation.js
+++ src/components/navigations/desktopnavigation/DesktopNavigation.js
@@ -5,10 +5,13 @@ export const DesktopNavigation = () => {
 
 const history = useHistory()
 
+const date = new Date()
+const todaysDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+const message = {welcomeMessage: "Welcome, todays date is", date: todaysDate}
     return (
         <div>
             <button onClick={ () => history.push(RoutingPath.homeView)}>Home</button>
-            <button onClick={ () => history.push(RoutingPath.todaysDateView)}>Todays Date</button>
+            <button onClick={ () => history.push({pathname: RoutingPath.todaysDateView, state: message})}>Todays Date</button>
         </div>
     )
 }

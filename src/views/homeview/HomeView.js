c5018ba1fd30519fdf8f96d464a32a43e841c939
Andrei Kozel <kozel.andrei.94@gmail.com>
2021-11-30 19:59:12 +0100

    fix: limit button width to content

--- src/views/homeview/HomeView.js
+++ src/views/homeview/HomeView.js
@@ -1,3 +1,4 @@
+import Button from "../../components/Button/Button";
 import { DisplayListOfPokemon } from "../../components/displaydata/displaypokemon/displaylistofpokemon/DisplayListOfPokemon";
 
 /**

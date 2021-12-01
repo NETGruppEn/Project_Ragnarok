0f7f17a59487aeabfdcf4ba8475071bd593e62c0
chrisbriz <christopher.brizet@outlook.com>
2021-12-01 13:34:22 +0100

    CardTitle component done

--- src/components/cardTitle/CardTitle.js
+++ src/components/cardTitle/CardTitle.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-const CardTitle = () => {
+const format = (id) => {
+  return id < 10 ? "#00" + id : id < 100 ? "#0" + id : "#" + id;
+}
+
+const capitalize = (name) => {
+  return name.charAt(0).toUpperCase() + name.slice(1);
+} 
+
+const CardTitle = ({id, name}) => {
   return (
     <div>
-      <p data-testid="id">#001</p>
-      <h3 data-testid="name">Bulbasaur</h3>
-      <span data-testid="star"></span>
+      <p data-testid="id">{format(id)}</p>
+      <h3 data-testid="name">{capitalize(name)}</h3>
     </div>
   );
 };

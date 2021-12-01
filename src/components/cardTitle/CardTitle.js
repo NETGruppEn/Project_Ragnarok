2845b31cbb43c49219570f02d729d55e6a6715bc
Emil Örjes <emil.orjes@hotmail.com>
2021-12-01 12:14:29 +0100

    Added CardTitle component and added test for name and id

---
+++ src/components/cardTitle/CardTitle.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CardTitle = () => {
+  return (
+    <div>
+      <p data-testid="id">#001</p>
+      <h3 data-testid="name">Bulbasaur</h3>
+      <span data-testid="star"></span>
+    </div>
+  );
+};
+
+export default CardTitle;

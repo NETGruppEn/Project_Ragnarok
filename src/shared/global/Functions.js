0613d1cdebb804d23adda858702234837e4e4cc9
Daudi88 <66510347+Daudi88@users.noreply.github.com>
2021-12-02 14:18:03 +0100

    Fix test in Info, divide Info into two functions, add XML comments to components (#7)
    Co-authored-by: Emil Örjes <emil.orjes@hotmail.com>

--- src/shared/global/Functions.js
+++ src/shared/global/Functions.js
@@ -1,3 +1,8 @@
+/**
+ * Switches the first letter to capital
+ * @param {string} text The text to capitalize the first letter
+ * @returns Text with first letter uppercase
+ */
 export const capitalize = (text) => {
   return text.charAt(0).toUpperCase() + text.slice(1);
 };

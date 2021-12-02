48d55ed607ab4a38f1922137132733e0072699ee
Malmsteg <73699667+Malmsteg@users.noreply.github.com>
2021-12-02 14:11:41 +0100

    Found a great looking pokemon logo

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    //TODO: Logo, line, pageTitle
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/header/Header.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Header from "./Header";
+
+// it("renders correctly with right title", () => {
+//     const div = document.createElement("div");
+//     render(<Header />, div);
+//     expect(screen.getByTestId("header").textContent).toBe(`© ${date.getFullYear()} Ragnarök inc`);
+// });

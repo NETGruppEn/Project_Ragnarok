ebf4590ace9db92c9a817cb553910a25f48ee496
Sammy Wong <66samwon@gafe.molndal.se>
2021-12-06 10:52:25 +0100

    Refactor Pagination

--- src/components/displaydata/displaypokemon/displaylistofpokemon/DisplayListOfPokemon.js
+++ src/components/displaydata/displaypokemon/displaylistofpokemon/DisplayListOfPokemon.js
@@ -4,8 +4,7 @@ import RoutingPath from "../../../../routes/RoutingPath";
 import { PokemonContext } from "../../../../shared/provider/PokemonProvider";
 import PokemonCard from "../../../pokemoncard/PokemonCard";
 import { DisplayLoading } from "../../displayloading/DisplayLoading";
-import Button from "../../../button/Button";
-import { COLORS } from "../../../../shared/global/Colors";
+import Pagination12 from "../../../pagination/pagination12/Pagination12";
 
 /**
  * DisplayListOfPokemon is a component that displays a list of pokemons with the help of the arrow function {displayData()} when used.
@@ -13,33 +12,28 @@ import { COLORS } from "../../../../shared/global/Colors";
  * @returns Arrow function {displayData()}.
  */
 export const DisplayListOfPokemon = () => {
-  const {allPokemon, AMOUNT_OF_POKEMON} = useContext(PokemonContext);
+  const { allPokemon, AMOUNT_OF_POKEMON } = useContext(PokemonContext);
   const [pokemon, setPokemon] = useState([]);
-  const [offset, setOffset] = useState(0);
+  const [offset, setOffset] = useState(876);
   const POKEMON_TO_SHOW = 12;
   const history = useHistory();
 
   useEffect(() => {
-    if (pokemon.length < 1 && allPokemon.length >= offset + POKEMON_TO_SHOW) {
-      getPokemonToShow();
+    if (
+      pokemon.length < 1 &&
+      (allPokemon.length >= offset + POKEMON_TO_SHOW ||
+        allPokemon.length == AMOUNT_OF_POKEMON)
+    ) {
+      setPokemon(allPokemon.slice(offset, offset + POKEMON_TO_SHOW));
     }
   });
 
   /**
-   * Gets the pokémon to show from the list of all pokémon based on the offset.
-   */
-  const getPokemonToShow = () => {
-    setPokemon(allPokemon.slice(offset, offset + POKEMON_TO_SHOW));
-  };
-
-  /**
-   *
    * @returns The component <DisplayLoading/> if no pokemon has loaded.
    * If some pokémon has been loaded, the pokémon cards are displayed.
-   *
    */
   const displayData = () => {
-    if (pokemon.length < POKEMON_TO_SHOW) {
+    if (pokemon.length < 1) {
       return <DisplayLoading />;
     }
 
@@ -52,40 +46,14 @@ export const DisplayListOfPokemon = () => {
     ));
   };
 
-  const getNextPokemon = () => {
-    if (offset + POKEMON_TO_SHOW < AMOUNT_OF_POKEMON) {
-      setOffset(offset + POKEMON_TO_SHOW);
-    } else {
-      setOffset(0);
-    }
-
-    setPokemon([]);
-  };
-
-  const getPrevPokemon = () => {
-    if (offset - POKEMON_TO_SHOW >= 0) {
-      setOffset(offset - POKEMON_TO_SHOW);
-    } else {
-      setOffset(AMOUNT_OF_POKEMON - POKEMON_TO_SHOW);
-    }
-
-    setPokemon([]);
-  };
-
   return (
     <div>
       {displayData()}
-      <Button
-        title="Prev"
-        onClick={() => getPrevPokemon()}
-        color={COLORS.blue}
-        onHover={COLORS.blueHover}
-      />
-      <Button
-        title="Next"
-        onClick={() => getNextPokemon()}
-        color={COLORS.blue}
-        onHover={COLORS.blueHover}
+      <Pagination12
+        offset={offset}
+        setOffset={setOffset}
+        POKEMON_TO_SHOW={POKEMON_TO_SHOW}
+        setPokemon={setPokemon}
       />
     </div>
   );
---
+++ src/components/pagination/pagination12/Pagination12.js
@@ -0,0 +1,48 @@
+import { useContext } from "react";
+import {COLORS} from "../../../shared/global/Colors";
+import { PokemonContext } from "../../../shared/provider/PokemonProvider";
+import Button from "../../button/Button";
+
+
+
+
+const Pagination12 = ({offset, setOffset, POKEMON_TO_SHOW, setPokemon}) => {
+    const {AMOUNT_OF_POKEMON}  = useContext(PokemonContext);
+    
+    const showNextPokemon = () => {
+        if (offset + POKEMON_TO_SHOW < AMOUNT_OF_POKEMON) {
+          setOffset(offset + POKEMON_TO_SHOW);
+        } else {
+          setOffset(0);
+        }
+    
+        setPokemon([]);
+      };
+    
+      const showPrevPokemon = () => {
+        if (offset - POKEMON_TO_SHOW >= 0) {
+          setOffset(offset - POKEMON_TO_SHOW);
+        } else {
+          setOffset(AMOUNT_OF_POKEMON - POKEMON_TO_SHOW);
+        }
+    
+        setPokemon([]);
+      };
+    
+    return (<div>
+         <Button
+        title="Prev"
+        onClick={() => showPrevPokemon()}
+        color={COLORS.blue}
+        onHover={COLORS.blueHover}
+      />
+      <Button
+        title="Next"
+        onClick={() => showNextPokemon()}
+        color={COLORS.blue}
+        onHover={COLORS.blueHover}
+      />
+    </div>)
+
+}
+export default Pagination12;

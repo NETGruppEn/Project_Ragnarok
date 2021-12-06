17e088b9169394a4b8167a575e88913d7feeffc0
Sammy Wong <66samwon@gafe.molndal.se>
2021-12-06 11:10:27 +0100

    Add css to Pagination component

--- src/components/pagination/pagination12/Pagination12.js
+++ src/components/pagination/pagination12/Pagination12.js
@@ -1,36 +1,35 @@
 import { useContext } from "react";
-import {COLORS} from "../../../shared/global/Colors";
+import { COLORS } from "../../../shared/global/Colors";
 import { PokemonContext } from "../../../shared/provider/PokemonProvider";
 import Button from "../../button/Button";
+import "./Pagination12.css";
 
+const Pagination12 = ({ offset, setOffset, POKEMON_TO_SHOW, setPokemon }) => {
+  const { AMOUNT_OF_POKEMON } = useContext(PokemonContext);
 
+  const showNextPokemon = () => {
+    if (offset + POKEMON_TO_SHOW < AMOUNT_OF_POKEMON) {
+      setOffset(offset + POKEMON_TO_SHOW);
+    } else {
+      setOffset(0);
+    }
 
+    setPokemon([]);
+  };
 
-const Pagination12 = ({offset, setOffset, POKEMON_TO_SHOW, setPokemon}) => {
-    const {AMOUNT_OF_POKEMON}  = useContext(PokemonContext);
-    
-    const showNextPokemon = () => {
-        if (offset + POKEMON_TO_SHOW < AMOUNT_OF_POKEMON) {
-          setOffset(offset + POKEMON_TO_SHOW);
-        } else {
-          setOffset(0);
-        }
-    
-        setPokemon([]);
-      };
-    
-      const showPrevPokemon = () => {
-        if (offset - POKEMON_TO_SHOW >= 0) {
-          setOffset(offset - POKEMON_TO_SHOW);
-        } else {
-          setOffset(AMOUNT_OF_POKEMON - POKEMON_TO_SHOW);
-        }
-    
-        setPokemon([]);
-      };
-    
-    return (<div>
-         <Button
+  const showPrevPokemon = () => {
+    if (offset - POKEMON_TO_SHOW >= 0) {
+      setOffset(offset - POKEMON_TO_SHOW);
+    } else {
+      setOffset(AMOUNT_OF_POKEMON - POKEMON_TO_SHOW);
+    }
+
+    setPokemon([]);
+  };
+
+  return (
+    <div className="pagination12">
+      <Button
         title="Prev"
         onClick={() => showPrevPokemon()}
         color={COLORS.blue}
@@ -42,7 +41,7 @@ const Pagination12 = ({offset, setOffset, POKEMON_TO_SHOW, setPokemon}) => {
         color={COLORS.blue}
         onHover={COLORS.blueHover}
       />
-    </div>)
-
-}
+    </div>
+  );
+};
 export default Pagination12;

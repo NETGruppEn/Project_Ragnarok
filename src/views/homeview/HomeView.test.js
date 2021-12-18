24500233944cb966ea393f4b44b4b2df25422c08
Sammy Wong <66samwon@gafe.molndal.se>
2021-12-18 14:07:02 +0100

    another try

--- src/views/homeview/HomeView.test.js
+++ src/views/homeview/HomeView.test.js
@@ -4,7 +4,7 @@ import HomeView from "./HomeView";
 import PokemonProvider from "../../shared/provider/PokemonProvider";
 
 describe("integration test 1", () => {
-  it("Search for pokemon \"Weedle\" and make sure it is returned", () => {
+  it('Search for pokemon "Weedle" and make sure it is returned', () => {
     render(
       <PokemonProvider>
         <HomeView />
@@ -13,9 +13,27 @@ describe("integration test 1", () => {
     const inputElement = screen.getByRole("textbox");
     const buttonElement = screen.getByTestId("search");
     fireEvent.change(inputElement, { target: { value: "Weedle" } });
+    //console.log(inputElement);
     fireEvent.click(buttonElement);
-    const divElement = async () => {
-      await screen.getByAltText("Weedle");
+    const divElement = () => {
+      screen.getByAltText(/Weedle/i);
     };
+    //console.log(divElement);
   });
 });
+
+// describe("integration test 2", () => {
+//   it('Click on pokemon "Bulbasaur" and go to its detail page', () => {
+//     // render(
+//     //   <PokemonProvider>
+//     //     <HomeView />
+//     //   </PokemonProvider>
+//     // );
+//     const divElement = async () => {
+//       await screen.findByAltText("Bulbasaur");
+//     };
+//     //fireEvent.click(divElement);
+
+    
+//   });
+// });
